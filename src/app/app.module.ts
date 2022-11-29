bc8ca1298006504ecc597f3f986a4f684494eb9b
MDavidBenoit <msanchez@desarrollom.systems>
2022-11-29 09:54:09 -0600

    carga de proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { Geolocation } from '@awesome-cordova-plugins/geolocation/ngx';
+import { NativeGeocoder, NativeGeocoderResult, NativeGeocoderOptions } from '@awesome-cordova-plugins/native-geocoder/ngx';
+
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [Geolocation,NativeGeocoder,{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {AlertController, ModalController } from '@ionic/angular';
+import { TranslateModule } from '@ngx-translate/core';
+// import {ApiService} from 'src/app/services/api.service';
+import { Plugins }  from '@capacitor/core';
+const {Geolocation} = Plugins;
+import { NativeGeocoder, NativeGeocoderResult, NativeGeocoderOptions } from '@awesome-cordova-plugins/native-geocoder/ngx';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  locations:any=[];
+  location = localStorage.getItem("location");
+  locationSearched:any;
+  locationSelected:any;
+  coords:any;
+
+  constructor(
+    private modal: ModalController,
+    private apiService: ApiService,
+    private translateService: TranslateModule, 
+    private nativeGeocoder: NativeGeocoder,
+    public alertController: AlertController,
+    ) {
+
+    }
+  
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  closeModal(){
+    this.modal.dismiss();
+  }
+  
+  async locate(){
+    const coordinates = await this.Geolocation.getCurrentPosition();
+    this.coords = coordinates.coords;
+    console.log("Coords: ", this.coords)
+    this.getCity(this.coords.latitude, this.coords.longitude)
+  }
+
+  getCity(latitude: number, longitude: number){
+    let options: NativeGeocoderOptions = {
+      useLocale: true,
+      maxResults: 5
+    };
+
+    this.nativeGeocoder.reverseGeocode(latitude, longitude, options)
+    .then((result: NativeGeocoderResult[]) => 
+    this.saveCity(result))
+    .catch((error:any)=>
+    this.presentAlert1());
+    
+  }
+
+  saveCity(result){
+    console.log(JSON.stringify(result[0].locality))
+    this.cityFounded(JSON.stringify(result[0].locality))
+  }
 
 }
+
+
